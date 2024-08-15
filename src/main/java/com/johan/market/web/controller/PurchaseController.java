fc179c704a6547cc0cfd11ea9f94858b281719a1
Johan <johanbcd@gmail.com>
2024-08-15 20:33:34 +0200

    finish first version

--- src/main/java/com/johan/market/web/controller/PurchaseController.java
+++ src/main/java/com/johan/market/web/controller/PurchaseController.java
@@ -2,6 +2,9 @@ package com.johan.market.web.controller;
 
 import com.johan.market.domain.Purchase;
 import com.johan.market.domain.service.PurchaseService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,11 +18,17 @@ public class PurchaseController {
     private PurchaseService purchaseService;
 
     @GetMapping("/all")
+    @Operation(summary = "Get all purchases")
     public ResponseEntity<List<Purchase>> getAll(){
         return new ResponseEntity<>(purchaseService.getAll(), HttpStatus.OK);
     }
+    @Operation(summary = "Get purchases by client id")
+    @ApiResponse(responseCode = "200", description = "Client ok")
+    @ApiResponse(responseCode = "404", description = "Client not found")
     @GetMapping("client/{id}")
-    public ResponseEntity<List<Purchase>> getByClientId(@PathVariable("id") String clientId){
+    public ResponseEntity<List<Purchase>> getByClientId(
+            @Parameter(description = "Client Id", required = true, example = "4546221")
+            @PathVariable("id") String clientId){
         return purchaseService.getByClient(clientId).map(
                 purchases -> new ResponseEntity<>(purchases, HttpStatus.OK))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
